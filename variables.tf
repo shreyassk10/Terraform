869eb3f69a71233baeb9d1ab9ea2542f87648766
shreyassk10 <167885224+shreyassk10@users.noreply.github.com>
2024-04-23 20:29:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "rg1"
+}
+
+variable "location" {
+  description = "East US"
+}
+
+variable "address_space" {
+  description = "The name of the Subnet within the Virtual Network."
+}
+
+variable "app_service_plan_name" {
+  description = "The name of the App Service Plan."
+}
+
+variable "app_service_name" {
+  description = "The name of the App Service."
+}

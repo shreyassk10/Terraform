879201123a9c025a753543bfe7b65b43b05ea252
shreyassk10 <167885224+shreyassk10@users.noreply.github.com>
2024-04-23 21:25:32 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,30 @@
 variable "resource_group_name" {
+  type = string
   description = "rg1"
 }
 
 variable "location" {
+  type = string
   description = "East US"
 }
 
 variable "address_space" {
+  type = string
   description = ["10.0.0.0/16"]
 }
 
 variable "address_prefixes" {
+  type = string
   description = ["10.0.0.0/22"]
 }
 
 variable "app_service_plan_name" {
+  type = string
   description = "test_01"
 }
 
 variable "sku_tier" {
+  type = string
   description = "Standard"
 }
 

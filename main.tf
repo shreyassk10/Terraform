49efab9f2b186e06eff1aee4abbf85358b4a4e1b
shreyassk10 <167885224+shreyassk10@users.noreply.github.com>
2024-04-23 20:56:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "var.resource_group_name"
   location = "var.location"
 }
-
 resource "azurerm_virtual_network" "vnet" {
-  name                = "var.vnet"
+  name                = "vnet1"
   address_space       = "var.address"
   location            = "var.location"
   resource_group_name = "var.resource_group_name"
 }  
 resource "azurerm_subnet" "example" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/22"]
+  name                 = "sub1"
+  resource_group_name  = "var.resource_group_name"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
+  address_prefixes     = "var.address_prefixes"
 }
 resource "azurerm_app_service_plan" "svcpln" {
   name                = "var.app_service_plan_name"

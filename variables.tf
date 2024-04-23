92fb0a1b3cbd078c004520952d37751f2cec1216
shreyassk10 <167885224+shreyassk10@users.noreply.github.com>
2024-04-23 20:51:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,10 @@ variable "address_space" {
   description = ["10.0.0.0/16"]
 }
 
+variable "address_prefixes" {
+  description = ["10.0.0.0/22"]
+}
+
 variable "app_service_plan_name" {
   description = "test_01"
 }
@@ -21,7 +25,3 @@ variable "sku_tier" {
 variable "sku_size" {
   description = "S1"
 }
-
-change
-vnet name
-subnet name
